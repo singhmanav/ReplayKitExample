9ee8dcd2532d5d955096d4620674f9e56af420a2
M <manav@xebia.com>
2019-08-27 16:28:33 +0530

    Initial Commit

---
+++ ReplayKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ReplayKitExample
+//
+//  Created by xeadmin on 27/08/19.
+//  Copyright © 2019 Manav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
